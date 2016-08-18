9b315302f1181c2d629a038690df16cf18d056c2
vibhakarsaraswat <vibhakar.saraswat@gmail.com>
2016-08-19 01:51:51 +0200

    Updated the APP as per review comments i.e.
    1. In order to Display "Current Local Time" in the 'SleepActivity' so that user can decide how long s/he wants to listen more, before turning off the music play, added a new 'Library Dependency' i.e. 'joda-time:joda-time:2.9.4' which will fetch the "Current Local Time" and display to the user.
    
    2. Added info TextView for each Activity.
    
    3. Moved color declarations to 'color.xml'

--- app/src/main/java/com/example/android/rockloud/SleepActivity.java
+++ app/src/main/java/com/example/android/rockloud/SleepActivity.java
@@ -1,10 +1,16 @@
 package com.example.android.rockloud;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.RelativeLayout;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.joda.time.LocalTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 public class SleepActivity extends AppCompatActivity {
 
@@ -13,6 +19,31 @@ public class SleepActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep);
 
+
+        // Initializing a new Instance of 'LocalTime' to fetch Current LocalTime
+        final LocalTime rightNow = new LocalTime();
+
+        // Initializing a new Instance of 'DateTimeFormatter' to remove default MilliSeconds in 'LocalTime'.
+        final DateTimeFormatter fmt = DateTimeFormat.forPattern("HH:mm:ss");
+
+        /**
+         * Display the Current Local Time on Button Click
+         * 1. Find the Button (View) having ID = btnShowLocalDateTime
+         * 2. Set a clickListener on that View.
+         * 3. Overriding the onClick (Public Method) of View.OnClickListener Interface by
+         * 4. Finding the TextView having ID = txvShowLocalTime and then
+         * 5. Setting the Text equal to value present in 'rightNow' variable with Formatting as Current Local TIme
+         */
+        final Button objBTNSLDT = (Button) findViewById(R.id.btnShowLocalTime);
+        objBTNSLDT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView objTXVSLDT = (TextView) findViewById(R.id.txvShowLocalTime);
+                objTXVSLDT.setText(fmt.print(rightNow));
+            }
+        });
+
+
         /**
          * DISPLAY TOAST MESSAGE FOR 'SleepActivity'
          * 1. Find the Parent View that shows the 'SleepActivity'
@@ -20,7 +51,7 @@ public class SleepActivity extends AppCompatActivity {
          * 3. Overriding the onClick (Public Method) of View.OnClickListener Interface by
          * 4. defining a 'TOAST' message to be displayed whenever the Parent View is clicked.
          */
-        RelativeLayout objSleepActivity = (RelativeLayout) findViewById(R.id.reloSleepActivity);
+        ImageView objSleepActivity = (ImageView) findViewById(R.id.imgSleep);
         objSleepActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
